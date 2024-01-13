5bea3e769f126ca5cb77ba583ba3880dec904661
Chwen <zzero.i.n.g@gmail.com>
2024-01-13 21:32:48 +0630

    slick error

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 import '../scss/style.scss';
+
+$import jQuery from 'jquery';
+$import 'slick-carousel';
+const $ = jQuery;
+
+$(document).ready(function(){
+    $('.card-wrap').slick({
+        fade: false,
+        autoplay: false,
+        pauseOnHover: true,
+        autoplaySpeed: 5400,
+        speed: 300,
+        touchThreshold: 8,
+        infinite: false,
+        variableWidth: false,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        arrows: true,
+        dots: false
+    });
+});
+
+
+
