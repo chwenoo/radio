aef55a5692884c4307ac2657550100d9f8136850
Chwen <zzero.i.n.g@gmail.com>
2024-01-15 15:19:07 +0630

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import '../scss/style.scss';
+// import '@fortawesome/fontawesome-free/js/all.js';
+// import '@fortawesome/fontawesome-free/css/all.css';
 
-$import jQuery from 'jquery';
-$import 'slick-carousel';
+import jQuery from 'jquery';
+import 'slick-carousel';
 const $ = jQuery;
 
 $(document).ready(function(){
@@ -12,13 +14,42 @@ $(document).ready(function(){
         autoplaySpeed: 5400,
         speed: 300,
         touchThreshold: 8,
-        infinite: false,
+        infinite: true,
         variableWidth: false,
         slidesToShow: 5,
-        slidesToScroll: 5,
+        slidesToScroll: 1,
         arrows: true,
-        dots: false
-    });
+        dots: false,
+        
+        responsive: [
+            {
+                breakpoint: 920,
+                settings: {
+                slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 680,
+                settings: {
+                slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                slidesToShow: 1
+                }
+            }
+        ]
+    });                    
+});
+
+// Responsive Navigation
+const hamburgerMenu = document.querySelector('.l-header__hamburger > a');
+const list  = document.querySelector('.l-header__list');
+
+hamburgerMenu.addEventListener('click', () => {
+    list.classList.toggle('show');
 });
 
 
